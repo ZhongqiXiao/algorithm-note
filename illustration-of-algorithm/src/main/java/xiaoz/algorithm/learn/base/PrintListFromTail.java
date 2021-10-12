0be00ca85bdb51a699cfc3a1755caf5864d24364
zouwanchen <zouwanchen@crc.com.hk>
2021-10-12 23:45:16 +0800

    refactor(base): [从尾到头打印链表] 空间复杂度优化

--- illustration-of-algorithm/src/main/java/xiaoz/algorithm/learn/base/PrintListFromTail.java
+++ illustration-of-algorithm/src/main/java/xiaoz/algorithm/learn/base/PrintListFromTail.java
@@ -10,14 +10,12 @@ import java.util.Stack;
 public class PrintListFromTail {
     public int[] reversePrint(ListNode head) {
         Stack<Integer> stack = new Stack<>();
-        ListNode p = head;
-        while (p != null) {
-            stack.push(p.val);
-            p = p.next;
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
         }
-        int size = stack.size();
-        int[] res = new int[size];
-        for (int i = 0; i < size; i++) {
+        int[] res = new int[stack.size()];
+        for (int i = 0; i < res.length; i++) {
             res[i] = stack.pop();
         }
         return res;
