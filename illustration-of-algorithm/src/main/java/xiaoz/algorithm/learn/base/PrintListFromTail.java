0a99d52aa690ba83e0e907f68063d537337aa59f
zouwanchen <zouwanchen@crc.com.hk>
2021-10-13 23:41:40 +0800

    refactor(base): 增加题目描述

--- illustration-of-algorithm/src/main/java/xiaoz/algorithm/learn/base/PrintListFromTail.java
+++ illustration-of-algorithm/src/main/java/xiaoz/algorithm/learn/base/PrintListFromTail.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * 剑指 Offer 06. 从尾到头打印链表
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * https://leetcode-cn.com/leetbook/read/illustration-of-algorithm/5dt66m/
+ */
 public class PrintListFromTail {
     public int[] reversePrint(ListNode head) {
         Stack<Integer> stack = new Stack<>();
--- illustration-of-algorithm/src/main/java/xiaoz/algorithm/learn/base/ReplaceString.java
+++ illustration-of-algorithm/src/main/java/xiaoz/algorithm/learn/base/ReplaceString.java
@@ -1,5 +1,10 @@
 package xiaoz.algorithm.learn.base;
 
+/**
+ * 剑指 Offer 05. 替换空格
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * 
+ */
 public class ReplaceString {
     public String replaceSpace(String s) {
         return s.replaceAll(" ", "%20");
